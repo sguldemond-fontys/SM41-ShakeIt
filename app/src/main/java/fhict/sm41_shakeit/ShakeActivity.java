aa3c55dd4ba4003eb8ddae1bbad4a89b66406434
sander <353331@student.fontys.nl>
2016-10-06 14:57:13 +0200

    Begin basic layout

---
+++ app/src/main/java/fhict/sm41_shakeit/ShakeActivity.java
@@ -0,0 +1,17 @@
+package fhict.sm41_shakeit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Sander on 6-10-2016.
+ */
+
+public class ShakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.shake);
+    }
+}
---
+++ app/src/main/java/fhict/sm41_shakeit/activityActivity.java
@@ -0,0 +1,16 @@
+package fhict.sm41_shakeit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+/**
+ * Created by Sander on 6-10-2016.
+ */
+
+public class activityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+    }
+}
