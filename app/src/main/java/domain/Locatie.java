044064c4223f9d1bb876de251ff91fca14ddf34d
sguldemond <s.guldemond@student.fontys.nl>
2016-10-20 12:56:17 +0200

    domain classes added shakeactivity test added

---
+++ app/src/main/java/domain/Locatie.java
@@ -0,0 +1,26 @@
+package domain;
+
+import android.location.Location;
+
+/**
+ * Created by stasiuz on 20/10/16.
+ */
+public class Locatie {
+    private String naam;
+    private String plaats;
+    private String postcode;
+    private String straat;
+    private String huisnummer;
+    private double lat;
+    private double lon;
+
+    public Locatie(String naam, String plaats, String postcode, String straat, String huisnummer, double lat, double lon) {
+        this.naam = naam;
+        this.plaats = plaats;
+        this.postcode = postcode;
+        this.straat = straat;
+        this.huisnummer = huisnummer;
+        this.lat = lat;
+        this.lon = lon;
+    }
+}
