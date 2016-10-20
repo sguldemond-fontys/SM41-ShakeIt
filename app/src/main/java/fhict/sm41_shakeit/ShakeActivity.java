4acd81b703c722a465877726cade75e0b5f450b9
sander <353331@student.fontys.nl>
2016-10-20 12:50:46 +0200

    Je kan nu terug gaan naar het options menu

--- app/src/main/java/fhict/sm41_shakeit/ShakeActivity.java
+++ app/src/main/java/fhict/sm41_shakeit/ShakeActivity.java
@@ -12,6 +12,8 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import java.text.SimpleDateFormat;
@@ -130,6 +132,26 @@ public class ShakeActivity extends AppCompatActivity implements LocationListener
     @Override
     public void onProviderDisabled(String provider) {
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(ShakeActivity.this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+        }
+        return false;
     }
 
     @Override
