e737dd482fb8fa4b900d7f5d7560c03bf9faa7b2
MvG013 <marcvangool@hotmail.com>
2016-11-08 15:18:45 +0100

    nog een commit

---
+++ app/src/main/java/fhict/sm41_shakeit/RegisterActivity.java
@@ -0,0 +1,74 @@
+package fhict.sm41_shakeit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import database.AsyncResponce;
+import database.BackgroundWorker;
+
+public class RegisterActivity extends AppCompatActivity implements AsyncResponce {
+
+    String bwType;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+
+        Button btnRegistreren = (Button)findViewById(R.id.btnInloggen);
+        Button btnTerug = (Button)findViewById(R.id.btnRegistreren);
+
+        btnRegistreren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etName = (EditText) findViewById(R.id.etName);
+                EditText etPassword = (EditText) findViewById(R.id.etPassword);
+                EditText etGeboortedatum = (EditText) findViewById(R.id.etGeboortedatum);
+
+                handleRegister(etName.getText().toString(), etPassword.getText().toString(),etGeboortedatum.getText().toString());
+            }
+        });
+
+        btnTerug.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleExit();
+            }
+        });
+    }
+
+    private void handleRegister(String name, String password , String geboortedatum) {
+        String[] input = new String[2];
+        input[0] = name;
+        input[1] = password;
+        input[2] = geboortedatum;
+
+        BackgroundWorker bw = new BackgroundWorker(this, this);
+
+        bwType = "Register";
+
+        bw.execute(bwType, input, null);
+        handleExit();
+    }
+
+    @Override
+    public void processFinish(String type, Object output) {
+        if(type.equals(bwType)) {
+            Boolean register = Boolean.parseBoolean((String)output);
+
+            if(register) {
+                handleExit();
+            }
+        }
+    }
+
+    private void handleExit() {
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
